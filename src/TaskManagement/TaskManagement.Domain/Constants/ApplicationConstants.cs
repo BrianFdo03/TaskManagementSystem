ceb5f8a29b82b9ac40c9f85e03583e0d9ef53358
Brian Fernando <132426362+BrianFdo03@users.noreply.github.com>
2026-07-06 11:38:30 +0530

    Initial Domain model and db setup

---
+++ src/TaskManagement/TaskManagement.Domain/Constants/ApplicationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagement.Domain.Constants;
+
+public static class ApplicationConstants
+{
+    public const string AdminRole = "Admin";
+    public const string EmployeeRole = "Employee";
+
+    public const string DefaultConnectionStringName = "DefaultConnection";
+
+    public const int JwtExpiryMinutes = 60;
+}
